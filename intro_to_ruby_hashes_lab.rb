d19cf37fbdd3e12b5f88ccf0fe1eaae2480467c0
Faith Janicek <faithe1937@gmail.com>
2019-09-25 18:00:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,14 +14,4 @@ def id_generator
   {:id = 4}
 end
 
-def my_hash_creator(key, value)
-  {key => value}
-end
 
-def read_from_hash(hash, key)
-  hash[key]
-end
-
-def update_counting_hash(hash, key)
- 
-end
