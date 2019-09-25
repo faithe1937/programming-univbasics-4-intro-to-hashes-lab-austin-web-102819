93941dc94434f2262182038db97bbd0ac96acb40
Faith Janicek <faithe1937@gmail.com>
2019-09-25 18:10:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,6 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  If hash[key]? hash[key] += 1 
-else 
-  hash[key] = 1 
+hash[key]? hash[key] += 1 hash[key] = 1 
 hash
 end 
